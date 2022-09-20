3f4631eda94e50be733e28660da8075bd53db3f5
bryanbits <bryanliu18@gmail.com>
2022-09-20 10:01:20 -0700

    add 9.1.7, 9.2.6

---
+++ 9/9.1/9.1.7/Book.java
@@ -0,0 +1,23 @@
+public class Book
+{
+    private int pages;
+    private String title;
+    
+    
+    public int getPages() {
+        return pages;
+    }
+    
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public void setTitle(String title){
+        this.title = title;
+    }
+    
+}
---
+++ 9/9.1/9.1.7/Mystery.java
@@ -0,0 +1,23 @@
+public class Mystery extends Book
+{
+    private String subgenre;
+    private String author;
+    
+    
+    public String getSubgenre() {
+        return subgenre;
+    }
+    
+    public void setSubgenre(String subgenre) {
+        this.subgenre = subgenre;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+}
