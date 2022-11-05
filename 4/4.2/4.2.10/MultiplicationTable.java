0cc268b8d9d88dfd175c2d674a3cd0fcfd563e86
cirraedon <bryanliu308@gmail.com>
2022-11-04 22:22:08 -0700

    4.2

---
+++ 4/4.2/4.2.10/MultiplicationTable.java
@@ -0,0 +1,9 @@
+public class MultiplicationTable
+{
+    public static void main(String[] args)
+    {
+        for (int i =1; i<=10; i++) {
+            System.out.println("4 * " + i + " = " + (4*i));
+        }
+    }
+}
---
+++ 4/4.2/4.2.7/Repeat150.java
@@ -0,0 +1,11 @@
+public class Repeat150
+{
+    public static void main(String[] args)
+    {
+        // this for loop does not follow good convention 
+        // ¯\_(ツ)_/¯
+        for (int i =150; i>=1; i--) {
+            System.out.println("Are we there yet?");
+        }
+    }
+}
