7c6b1b1b71655c9d63437522fad8810d7f5d53c8
cirraedon <114040455+cirraedon@users.noreply.github.com>
2022-12-31 11:39:18 -0800

    remove 4

--- 4/4.2/4.2.10/MultiplicationTable.java
+++
@@ -1,9 +0,0 @@
-public class MultiplicationTable
-{
-    public static void main(String[] args)
-    {
-        for (int i =1; i<=10; i++) {
-            System.out.println("4 * " + i + " = " + (4*i));
-        }
-    }
-}
--- 4/4.2/4.2.6/Odds.java
+++
@@ -1,9 +0,0 @@
-public class Odds
-{
-    public static void main(String[] args)
-    {
-        for (int i =1; i<100; i+=2) {
-            System.out.println(i);
-        }
-    }
-}
